7c8fc2359db6e371b7ba7e879707b639182ed504
Kedar <kedar@Kedars-MacBook-Air.local>
2025-06-25 13:01:49 +0530

    feat: Add higher-density grid templates

--- scripts/templates/grid-templates.js
+++ scripts/templates/grid-templates.js
@@ -1,12 +1,12 @@
-// A list of valid, pre-designed 6x6 grid layouts.
+// A list of valid, high-density 6x6 grid layouts.
 // '1' represents a letter, '0' represents a black square.
 export const templates = [
-  // Template 1: The "DELHI" Sample
-  ["111110", "101010", "101010", "101010", "011110", "010010"],
-  // Template 2: Open Center
-  ["111111", "100101", "101101", "101101", "101001", "111111"],
-  // Template 3: Diagonal Symmetry
-  ["111100", "111110", "110011", "011001", "011111", "001111"],
-  // Template 4: Windmill
+  // Template A: High Density (12 words)
+  ["111011", "111011", "111111", "011011", "110110", "110111"],
+  // Template B: Symmetrical High Density (10 words)
+  ["111110", "101011", "111101", "101111", "110101", "011111"],
+  // Template C: Original "Windmill" style (8 words)
   ["111000", "111011", "100011", "110001", "110111", "000111"],
+  // Template D: Open Corners (10 words)
+  ["011110", "101011", "110101", "101011", "110101", "011110"],
 ];
